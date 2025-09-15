93e2d2ae2e2bd5684c8b0969db04d678d3f39022
Mallika Kannan <mallika.kannan93@gmail.com>
2025-09-15 12:52:25 +0530

    day_9 changes

---
+++ day_9/processchecker.py
@@ -0,0 +1,16 @@
+import psutil 
+
+def check_process (process_name): 
+    for process in psutil.process_iter (['pid' , 'name']):
+        if process_name.lower() in process.info ['name'].lower():
+            return True
+    return False
+
+if __name__ == "__main__":
+ process_to_check = "nginx"
+ if check_process (process_to_check):
+    print(f"The process '{process_to_check}' is running.")
+ else : 
+    print(f"The process '{process_to_check}' is NOT running.")
+
+
