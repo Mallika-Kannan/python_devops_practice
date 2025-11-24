7301762f641bb7ada454ba4a5190507c998ecaff
Mallika Kannan <mallika.kannan93@gmail.com>
2025-11-24 21:30:03 +0530

    Day_20 changes on slack messsage from Python script using webook_1

--- day_20/slackrequests.py
+++ day_20/slackrequests.py
@@ -9,7 +9,7 @@ def send_slack_message(webhook_url, message):
         print ("Message not sent", response.txt)
 if __name__ == "__main__":
     
-    WEBHOOK_URL = "https://hooks.slack.com/services/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"
+    WEBHOOK_URL = "XXXX"
     MESSAGE = "Hello from my slack python script"
 
     send_slack_message(WEBHOOK_URL, MESSAGE)
